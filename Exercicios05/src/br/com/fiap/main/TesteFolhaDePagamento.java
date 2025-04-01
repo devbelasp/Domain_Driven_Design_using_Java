4a43f7b7c0c840288ac009b24ea0c8b976254681
devbelasp <isa.bela360@hotmail.com>
2025-04-01 20:42:33 -0300

    Exercicio 05

--- Exercicios05/src/br/com/fiap/main/TesteFolhaDePagamento.java
+++ Exercicios05/src/br/com/fiap/main/TesteFolhaDePagamento.java
@@ -12,7 +12,7 @@ public class TesteFolhaDePagamento {
         salario.numeroDeDependentes = 2;
         salario.descontoINSS = 12;
         double resultado = salario.calcularSalarioLiquido();
-        String mensagem = String.format("Teste Folha De Pagamento\nSalário Bruto: %.2f\nDesconto do plano de saúde: %.2f * 3 (2 pessoas como dependentes + funcionário)\nDesconto do INSS: %.0f pocento\nSalário Líquido: %.2f", salario.salarioBruto, salario.valorPlanoDeSaude, salario.descontoINSS, resultado);
+        String mensagem = String.format("Teste Folha De Pagamento\nSalário Bruto: %.2f\nDesconto Plano De Saúde: %.2f\nNúmero De Dependentes: %d\nDesconto Do INSS: %.0f porcento\nSalário Líquido: %.2f", salario.salarioBruto, salario.valorPlanoDeSaude, salario.numeroDeDependentes, salario.descontoINSS, resultado);
         JOptionPane.showMessageDialog(null,mensagem);
 
 
