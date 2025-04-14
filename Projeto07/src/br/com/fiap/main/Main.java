830e32a4d20053266e28a63dbdebd9458fc61dda
Isabela Santos <isa.bela360@hotmail.com>
2025-04-14 09:15:06 -0300

    Projeto aula 07

---
+++ Projeto07/src/br/com/fiap/main/Main.java
@@ -0,0 +1,17 @@
+package br.com.fiap.main;
+
+import br.com.fiap.bean.Radio;
+
+public class Main {
+    public static void main(String[] args) {
+        // declarando e instanciando o objeto da classe Radio
+        Radio radio = new Radio();
+        radio.setVolume(99);
+        radio.setEstacao(189.1f);
+        radio.aumentarVolume();
+        radio.aumentarVolume();
+        radio.aumentarVolume();
+        radio.setEstacao(192.5f);
+        System.out.printf("Estação: %.1fMHz \nVolume: %ddB", radio.getEstacao(),radio.getVolume());
+    }
+}
