cb3ef63774a5529162f294c6b58dc7ef2d6ea53d
devbelasp <isa.bela360@hotmail.com>
2025-04-26 15:59:24 -0300

    cp2 java

--- CP2/src/br/com/fiap/bean/Aluno.java
+++ CP2/src/br/com/fiap/bean/Aluno.java
@@ -1,5 +1,6 @@
 package br.com.fiap.bean;
 
+import javax.swing.*;
 import java.time.LocalDate;
 
 public class Aluno {
@@ -22,13 +23,13 @@ public class Aluno {
     }
     public void setResgistroMatricula(int resgistroMatricula) {
         try {
-            if (resgistroMatricula >= 80000 && resgistroMatricula <= 599999){
+            if (resgistroMatricula >= 80000 && resgistroMatricula <= 599999) {
                 this.resgistroMatricula = resgistroMatricula;
             } else {
                 throw new Exception("Número de matrícula inválido!(min=80000 até max=599999)");
             }
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
     public String getNomeCompleto() {
@@ -41,21 +42,20 @@ public class Aluno {
         return anoDeNascimento;
     }
     public void setAnoDeNascimento(int anoDeNascimento) {
-        int anoAtual = LocalDate.now().getYear();
+        LocalDate dataAtual = LocalDate.now();
         try {
-            if (anoDeNascimento >= 1945 && anoDeNascimento <= anoAtual) {
+            if (anoDeNascimento >= 1945 && anoDeNascimento <= dataAtual.getYear()) {
                 this.anoDeNascimento = anoDeNascimento;
             } else {
                 throw new Exception("Ano de nascimento inválido! (min=1945 até max=Ano Atual)");
             }
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
     // métodos de classe (particulares)
     public int calcularIdade (LocalDate dataAtual) {
         return dataAtual.getYear() - anoDeNascimento;
-
     }
 
 }
--- CP2/src/br/com/fiap/main/MainAluno.java
+++ CP2/src/br/com/fiap/main/MainAluno.java
@@ -15,33 +15,28 @@ public class MainAluno {
         Aluno aluno3;
         Aluno aluno4;
 
-        LocalDate dataAtual = LocalDate.now();
-        String auxiliar;
+        LocalDate dataAtual = LocalDate.now(); // variável local (data atual)
 
         try {
-            // Aluno 3
-            aluno3 = new Aluno();
-            auxiliar = JOptionPane.showInputDialog("Digite o RM do aluno 3:");
-            aluno3.setResgistroMatricula(Integer.parseInt(auxiliar));
+            aluno3 = new Aluno();  // Instânciando objeto aluno3
+            // InputDialog - dados do aluno 3 fornecidos pelo usuário
+            aluno3.setResgistroMatricula(Integer.parseInt(JOptionPane.showInputDialog("Digite o RM do aluno 3:")));
             aluno3.setNomeCompleto(JOptionPane.showInputDialog("Digite o nome completo do aluno 3:"));
-            auxiliar = JOptionPane.showInputDialog("Digite o ano de nascimento do aluno 3:");
-            aluno3.setAnoDeNascimento(Integer.parseInt(auxiliar));
+            aluno3.setAnoDeNascimento(Integer.parseInt(JOptionPane.showInputDialog("Digite o ano de nascimento do aluno 3:")));
 
-            // Aluno 4
-            aluno4 = new Aluno();
-            auxiliar = JOptionPane.showInputDialog("Digite o RM do aluno 4:");
-            aluno4.setResgistroMatricula(Integer.parseInt(auxiliar));
+            aluno4 = new Aluno(); // Instânciando objeto aluno4
+            // InputDialog - dados do aluno 4 fornecidos pelo usuário
+            aluno4.setResgistroMatricula(Integer.parseInt(JOptionPane.showInputDialog("Digite o RM do aluno 4:")));
             aluno4.setNomeCompleto(JOptionPane.showInputDialog("Digite o nome completo do aluno 4:"));
-            auxiliar = JOptionPane.showInputDialog("Digite o ano de nascimento do aluno 4:");
-            aluno4.setAnoDeNascimento(Integer.parseInt(auxiliar));
+            aluno4.setAnoDeNascimento(Integer.parseInt(JOptionPane.showInputDialog("Digite o ano de nascimento do aluno 4:")));
 
-            // Exibindo mensagem
+            // Exibindo mensagem incluindo as informações dos 4 alunos
             String mensagem = String.format("Informações dos 4 integrantes do grupo: \nRM: %d | Aluno(a): %s | Idade: %d \nRM: %d | Aluno(a): %s | Idade: %d \nRM: %d | Aluno(a): %s | Idade: %d \nRM: %d | Aluno(a): %s | Idade: %d", aluno1.getResgistroMatricula(), aluno1.getNomeCompleto(), aluno1.calcularIdade(dataAtual), aluno2.getResgistroMatricula(), aluno2.getNomeCompleto(), aluno2.calcularIdade(dataAtual), aluno3.getResgistroMatricula(), aluno3.getNomeCompleto(), aluno3.calcularIdade(dataAtual), aluno4.getResgistroMatricula(), aluno4.getNomeCompleto(), aluno4.calcularIdade(dataAtual));
 
             JOptionPane.showMessageDialog(null,mensagem);
 
         } catch (Exception e) {
-            System.out.println("Nome, matrícula ou ano de nascimento inválido(s)");
+            JOptionPane.showMessageDialog(null,"Nome, matrícula ou ano de nascimento inválido(s)");
         }
     }
 }
